8cff8b727d84cce9feb8ab87c140bded5eebc0a7
Mehedi Hasan <mehedi08h@gmail.com>
2022-09-18 11:25:02 +0600

    HTTP response

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,12 +4,16 @@ import dotenv from "dotenv";
 import ip from "ip";
 
 import { connectDatabase } from "../src/config/database";
+import { Code } from "./enum/code.enum";
+import { HttpResponse } from "./domain/response";
+import { Status } from "./enum/status.enum";
 
 dotenv.config();
 
 export class App {
     private readonly app: Application;
     private readonly APPLICATION_RUNNING = "Application is running on:";
+    private readonly ROUTE_NOT_FOUND = "Route does not exist on this server.";
 
     constructor(
         private readonly port: string | number = process.env.SERVER_PORT || 5000
@@ -37,7 +41,24 @@ export class App {
     // routes
     private routes(): void {
         this.app.get("/", (req: Request, res: Response) => {
-            res.status(200).send({ message: "Welcome to Photo Share app." });
+            res.status(Code.OK).send(
+                new HttpResponse(
+                    Code.OK,
+                    Status.OK,
+                    "Welcome to Photo Share app."
+                )
+            );
+        });
+
+        // not found
+        this.app.all("*", (req: Request, res: Response) => {
+            res.status(Code.NOT_FOUND).send(
+                new HttpResponse(
+                    Code.NOT_FOUND,
+                    Status.NOT_FOUND,
+                    this.ROUTE_NOT_FOUND
+                )
+            );
         });
     }
 
--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -87,11 +87,16 @@ UserSchema.methods.getSignedToken = function (password: string) {
 
 // Generate password reset token
 UserSchema.methods.getResetPasswordToken = function () {
+    // Generate token
     const resetToken = crypto.randomBytes(20).toString("hex");
+
+    // Hash and set to resetPasswordToken
     this.resetPasswordToken = crypto
         .createHash("sha256")
         .update(resetToken)
         .digest("hex");
+
+    // Set token expire time
     this.resetPasswordExpire = Date.now() + 10 * (60 * 1000);
     return resetToken;
 };
